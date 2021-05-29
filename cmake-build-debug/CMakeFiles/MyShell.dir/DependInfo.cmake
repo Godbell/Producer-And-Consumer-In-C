b9c764595c074127d4ad8b8318a8a42346818a87
GodBell <12191579@inha.edu>
2021-05-29 12:16:45 +0900

    20210529 1216 modify gitignore

---
+++ cmake-build-debug/CMakeFiles/MyShell.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/e/Programming/4_C/Synch/synch_pthread.c" "/cygdrive/e/Programming/4_C/Synch/cmake-build-debug/CMakeFiles/MyShell.dir/synch_pthread.c.o"
+  "/cygdrive/e/Programming/4_C/Synch/synch_semaphore.c" "/cygdrive/e/Programming/4_C/Synch/cmake-build-debug/CMakeFiles/MyShell.dir/synch_semaphore.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MyShell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyShell.dir/synch_pthread.c.o"
+  "CMakeFiles/MyShell.dir/synch_semaphore.c.o"
+  "MyShell.exe"
+  "MyShell.pdb"
+  "libMyShell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyShell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
